4e557ee53d508780f470aeed54ea3c400694e7a7
D.  K.  Mariano <144285758+DKMariano@users.noreply.github.com>
2024-07-10 17:25:28 -0300

    Delete Python/Passo 01 - Sequências Básicas directory

--- Python/Passo 01 - Sequências Básicas/EX001/Ex001.py
+++
@@ -1,9 +0,0 @@
-# Data: 24/06/2024
-# Exercício: 001 - Exercícios de Algoritmos  (em Python)
-# Instituição de Ensino: Estudonauta
-# Disciplina: Python
-# Professor: Gustavo Guanabara
-# Aluno: D. K. Mariano
-# Descrição: Escreva um programa que mostre na tela a mensagem "Olá, Mundo!" na tela.
-
-print("Olá, Mundo!")
